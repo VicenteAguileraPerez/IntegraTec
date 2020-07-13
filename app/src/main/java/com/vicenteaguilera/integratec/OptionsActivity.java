080aea771fd7fc95a84fb3fd6bca3b0a39e1b27d
Salvador999 <salvadormorado2000@gmail.com>
2020-07-12 20:31:19 -0500

    [Mod] OptionsActivity

--- app/src/main/java/com/vicenteaguilera/integratec/OptionsActivity.java
+++ app/src/main/java/com/vicenteaguilera/integratec/OptionsActivity.java
@@ -35,11 +35,13 @@ public class OptionsActivity extends AppCompatActivity implements  View.OnClickL
         switch (id)
         {
             case R.id.button_Registrarse:
+                button_Registrarse.setTextColor(Color.YELLOW);
                 Toast.makeText(this, "Se dio clic en Registrarse", Toast.LENGTH_SHORT).show();
                 //Snackbar.make(Objects.requireNonNull(getCurrentFocus()), "Se dio clic en Registrarse.", Snackbar.LENGTH_SHORT).show();
                 break;
 
             case R.id.button_IniciarSesion:
+                button_IniciarSesion.setTextColor(Color.YELLOW);
                 Toast.makeText(this, "Se dio clic en Iniciar Sesión", Toast.LENGTH_SHORT).show();
                 //Snackbar.make(Objects.requireNonNull(getCurrentFocus()), "Se dio clic en Iniciar Sesión.", Snackbar.LENGTH_SHORT).show();
                 break;
