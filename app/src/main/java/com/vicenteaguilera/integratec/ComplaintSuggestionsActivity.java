c7010ca2d696e7ea7687f16b8f180ce37c8893d0
Antonio Pulido <antonio.pulidovirrueta@outlook.com>
2020-07-20 00:19:38 -0500

    [Add]ComplaintSuggestionsActivity

---
+++ app/src/main/java/com/vicenteaguilera/integratec/ComplaintSuggestionsActivity.java
@@ -0,0 +1,14 @@
+package com.vicenteaguilera.integratec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComplaintSuggestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complain_suggestions);
+    }
+}
