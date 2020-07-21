3dcf34069ae3887b5df29b5d00792a34c11fbeef
Salvador999 <salvadormorado2000@gmail.com>
2020-07-20 22:34:31 -0500

    [Mod] MainAdviserActivity

--- app/src/main/java/com/vicenteaguilera/integratec/MainAdviserActivity.java
+++ app/src/main/java/com/vicenteaguilera/integratec/MainAdviserActivity.java
@@ -1,8 +1,10 @@
 package com.vicenteaguilera.integratec;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainAdviserActivity extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class MainAdviserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_adviser);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.item_AcercaDe:
+                Toast.makeText(this, "Acerca de...", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.item_QuejasSugerencias:
+                Toast.makeText(this, "Quejas y sugerencias...", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.item_CerrarSesion:
+                Toast.makeText(this, "Cerrar sesión...", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
