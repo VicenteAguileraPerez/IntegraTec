e56fcaf15d2d90265cc13a42a600667a750ef2d4
VicenteAguileraPerez <vicente_prez@hotmail.com>
2020-07-22 10:38:07 -0500

    [Mod] Vista y controller, ajuste del scroll y agregar el TimePickerDialog, ajuste de los listeners apara el radiogroup, y editText de hora.

--- app/src/main/java/com/vicenteaguilera/integratec/MainAdviserActivity.java
+++ app/src/main/java/com/vicenteaguilera/integratec/MainAdviserActivity.java
@@ -1,7 +1,11 @@
 package com.vicenteaguilera.integratec;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,66 +16,55 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class MainAdviserActivity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Objects;
 
+public class MainAdviserActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private  TimePickerDialog picker=null;
     private Spinner spinner_materias;
     private Spinner spinner_lugares;
     private RadioGroup radioGroup;
-    private TextView textView_URL;
+    private EditText editText_URL;
     private EditText editText_HoraInicio;
     private EditText editText_HoraFinalizacion;
     private EditText editTextTextMultiLine;
     private Switch switchEstado;
 
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_adviser);
+        ActionBar toolbar = getSupportActionBar();
+        Objects.requireNonNull(toolbar).setElevation(0);
+        setTitle("Menú Asesor");
 
         spinner_materias = findViewById(R.id.spinner_Materia);
         spinner_lugares = findViewById(R.id.spinner_Lugar);
         radioGroup = findViewById(R.id.radioGroup);
-        textView_URL = findViewById(R.id.textView_URL);
+        editText_URL = findViewById(R.id.editView_URL);
+
         editText_HoraInicio = findViewById(R.id.editText_HoraInicio);
+        editText_HoraInicio.setInputType(InputType.TYPE_NULL);
         editText_HoraFinalizacion = findViewById(R.id.editTextTime_Finalizacion);
+        editText_HoraFinalizacion.setInputType(InputType.TYPE_NULL);
+
+        editText_HoraInicio.setOnClickListener(this);
+        editText_HoraFinalizacion.setOnClickListener(this);
+
         editTextTextMultiLine = findViewById(R.id.editTextTextMultiLine);
         switchEstado = findViewById(R.id.switch_Estado);
+        editText_URL.requestFocus();
+        editTextFocusListener();
+        radioButtonListener();
 
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
 
-                RadioButton checkedRadioButton = (RadioButton)radioGroup.findViewById(i);
-                boolean isChecked = checkedRadioButton.isChecked();
-
-                if(isChecked)
-                {
-                    if(checkedRadioButton.getId()==R.id.radioButton_AOnline)
-                    {
-                        spinner_lugares.setVisibility(View.GONE);
-
-                        textView_URL.setVisibility(View.VISIBLE);
-                        spinner_materias.setVisibility(View.VISIBLE);
-                        editText_HoraInicio.setVisibility(View.VISIBLE);
-                        editText_HoraFinalizacion.setVisibility(View.VISIBLE);
-                        editTextTextMultiLine.setVisibility(View.VISIBLE);
-
-                    }else if(checkedRadioButton.getId()==R.id.radioButton_APresencial)
-                    {
-                        textView_URL.setVisibility(View.GONE);
-
-                        spinner_lugares.setVisibility(View.VISIBLE);
-                        spinner_materias.setVisibility(View.VISIBLE);
-                        editText_HoraInicio.setVisibility(View.VISIBLE);
-                        editText_HoraFinalizacion.setVisibility(View.VISIBLE);
-                        editTextTextMultiLine.setVisibility(View.VISIBLE);
-                    }
-                }
-
-            }
-        });
 
         ArrayAdapter<CharSequence> arrayAdapterLugares = ArrayAdapter.createFromResource(this, R.array.string_lugares, android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> arrayAdapterMaterias = ArrayAdapter.createFromResource(this, R.array.string_materias, android.R.layout.simple_spinner_item);
@@ -80,16 +73,16 @@ public class MainAdviserActivity extends AppCompatActivity {
         spinner_materias.setAdapter(arrayAdapterMaterias);
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.overflow, menu);
         return true;
     }
-
+    @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
         int id = item.getItemId();
-
         switch (id){
             case R.id.item_AcercaDe:
                 Toast.makeText(this, "Acerca de...", Toast.LENGTH_SHORT).show();
@@ -104,5 +97,138 @@ public class MainAdviserActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        int idView = view.getId();
+        if(idView==R.id.editText_HoraInicio)
+        {
+            if(picker!=null)
+            {
+                if (!picker.isShowing()) {
+                    getHora(editText_HoraInicio);
+                }
+            }
+            else
+            {
+                getHora(editText_HoraInicio);
+            }
+        }
+        else if(idView==R.id.editTextTime_Finalizacion)
+        {
+            if(picker!=null)
+            {
+                if (!picker.isShowing())
+                {
+                    getHora(editText_HoraFinalizacion);
+                }
+            }
+            else
+            {
+                getHora(editText_HoraFinalizacion);
+            }
+        }
+
+    }
+    private void editTextFocusListener()
+    {
+        editText_HoraInicio.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focus) {
+                if(focus)
+                {
+                    if (picker != null) {
+                        if (focus && !picker.isShowing()) {
+                            getHora(editText_HoraInicio);
+                        }
+                    } else {
+                        getHora(editText_HoraInicio);
+                    }
+                }
+            }
+        });
+        editText_HoraFinalizacion.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focus) {
+
+                if(focus)
+                {
+                    if (picker != null) {
+                        if (!picker.isShowing()) {
+
+                            getHora(editText_HoraFinalizacion);
+                        }
+                    } else {
+                        getHora(editText_HoraFinalizacion);
+                    }
+                }
+
+            }
+        });
+    }
+    private void radioButtonListener()
+    {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int radiobutton) {
+
+                RadioButton checkedRadioButton = radioGroup.findViewById(radiobutton);
+                boolean isChecked = checkedRadioButton.isChecked();
+
+                if(isChecked)
+                {
+                    if(checkedRadioButton.getId()==R.id.radioButton_AOnline)
+                    {
+                        spinner_lugares.setVisibility(View.GONE);
+                        editText_URL.setVisibility(View.VISIBLE);
+                    }else if(checkedRadioButton.getId()==R.id.radioButton_APresencial)
+                    {
+                        editText_URL.setVisibility(View.GONE);
+                        spinner_lugares.setVisibility(View.VISIBLE);
+                    }
+                }
+
+            }
+        });
+    }
+
+    private void getHora(final EditText view) {
+        if (picker != null)
+        {
+            if(picker.isShowing())
+            {
+                picker.cancel();
+            }
+            picker=null;
+        }
+        final Calendar cldr = Calendar.getInstance();
+        final int hour = cldr.get(Calendar.HOUR_OF_DAY);
+        int minutes = cldr.get(Calendar.MINUTE);
+        // time picker dialog
+
+
+        TimePickerDialog.OnTimeSetListener timePicker = new TimePickerDialog.OnTimeSetListener() {
+
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hrs, int min) {
+                String horas = hrs < 10 ? "0" + hrs : hrs + "";
+                String minutos = min < 10 ? "0" + min : min + "";
+                Toast.makeText(MainAdviserActivity.this, hrs + ":" + min, Toast.LENGTH_SHORT).show();
+                view.setText(horas + ":" + minutos);
+            }
+        };
+        if (picker == null) {
+            picker = new TimePickerDialog(MainAdviserActivity.this, timePicker, hour, minutes, true);
+        }
+        picker.show();
+
+
+
+
+
+
+
     }
 }
