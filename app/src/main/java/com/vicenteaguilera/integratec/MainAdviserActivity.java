47aee02dcd3779c006c8ab72500a3c09d3e33014
Salvador999 <salvadormorado2000@gmail.com>
2020-07-21 19:49:23 -0500

    [Mod] MainAdviserActivity [Add] opciones_spinner.xml

--- app/src/main/java/com/vicenteaguilera/integratec/MainAdviserActivity.java
+++ app/src/main/java/com/vicenteaguilera/integratec/MainAdviserActivity.java
@@ -4,14 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainAdviserActivity extends AppCompatActivity {
+
+    private Spinner spinner_materias;
+    private Spinner spinner_lugares;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_adviser);
+
+        spinner_materias = findViewById(R.id.spinner_Materia);
+        spinner_lugares = findViewById(R.id.spinner_Lugar);
+
+        ArrayAdapter<CharSequence> arrayAdapterLugares = ArrayAdapter.createFromResource(this, R.array.string_lugares, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> arrayAdapterMaterias = ArrayAdapter.createFromResource(this, R.array.string_materias, android.R.layout.simple_spinner_item);
+
+        spinner_lugares.setAdapter(arrayAdapterLugares);
+        spinner_materias.setAdapter(arrayAdapterMaterias);
     }
 
     public boolean onCreateOptionsMenu(Menu menu)
