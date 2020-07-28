55ba93e8cba1f1f542874a0cfbc9b942a75c524e
VicenteAguileraPerez <vicente_prez@hotmail.com>
2020-07-27 22:34:42 -0500

    [MOD] CornerRadius en 0dp

--- app/src/main/java/com/vicenteaguilera/integratec/OptionsActivity.java
+++ app/src/main/java/com/vicenteaguilera/integratec/OptionsActivity.java
@@ -8,9 +8,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.Objects;
 
 public class OptionsActivity extends AppCompatActivity implements  View.OnClickListener{
 
