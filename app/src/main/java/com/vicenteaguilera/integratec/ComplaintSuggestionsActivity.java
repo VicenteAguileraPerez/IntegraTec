50f101e814b1d73ae1934de50bfcc76195c7bf5a
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2020-07-29 09:43:01 -0500

    [MOD] cambio de los recursosString de ComplaintSuggestionsActivity.java y activity_complain_suggestions.xml.xml

--- app/src/main/java/com/vicenteaguilera/integratec/ComplaintSuggestionsActivity.java
+++ app/src/main/java/com/vicenteaguilera/integratec/ComplaintSuggestionsActivity.java
@@ -5,18 +5,52 @@ import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class ComplaintSuggestionsActivity extends AppCompatActivity implements View.OnClickListener {
-    CardView cardView_ButtonPublicar;
+    private CardView cardView_ButtonPublicar;
+    private RadioButton radioButton_queja,radioButton_sugerencia;
+    private RadioGroup radioGroup_topic;
+    String topic="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complain_suggestions);
-
+        radioGroup_topic = findViewById(R.id.radioGroup_topic);
         cardView_ButtonPublicar = findViewById(R.id.cardView_ButtonPublicar);
         cardView_ButtonPublicar.setOnClickListener(this);
+        radioButtonListener();
+    }
+    private void radioButtonListener()
+    {
+        radioGroup_topic.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int radiobutton) {
+
+                RadioButton checkedRadioButton = radioGroup.findViewById(radiobutton);
+                boolean isChecked = checkedRadioButton.isChecked();
+
+                if(isChecked)
+                {
+                    if(checkedRadioButton.getId()==R.id.radioButton_queja)
+                    {
+                       topic= getResources().getString(R.string.queja).toLowerCase();
+
+                    }else if(checkedRadioButton.getId()==R.id.radioButton_sugerencia)
+                    {
+                        topic= getResources().getString(R.string.sugerencia).toLowerCase();
+                    }
+                    else
+                    {
+                        topic="";
+                    }
+                }
+
+            }
+        });
     }
 
     @Override
@@ -25,7 +59,10 @@ public class ComplaintSuggestionsActivity extends AppCompatActivity implements V
 
         switch (id){
             case R.id.cardView_ButtonPublicar:
-                Snackbar.make(view,"¡Gracias, atenderemos pronto tu queja!",Snackbar.LENGTH_SHORT).show();
+                if(!topic.equals(""))
+                {
+                    Snackbar.make(view, R.string.agradecimientos + topic, Snackbar.LENGTH_SHORT).show();
+                }
                 break;
         }
     }
