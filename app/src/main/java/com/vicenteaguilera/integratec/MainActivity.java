dc03c0db19acd1204cfaf29e44759e85a70c7db0
Salvador999 <salvadormorado2000@gmail.com>
2020-08-04 23:46:46 -0500

    [Mod] MainActivity

--- app/src/main/java/com/vicenteaguilera/integratec/MainActivity.java
+++ app/src/main/java/com/vicenteaguilera/integratec/MainActivity.java
@@ -3,12 +3,26 @@ package com.vicenteaguilera.integratec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText_Nombre;
+    private EditText editText_Tema;
+    private EditText editText_Fecha;
+    private Spinner spinner_Carrera;
+    private Spinner spinner_Asignatura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText_Nombre = findViewById(R.id.editText_Nombre);
+        editText_Tema = findViewById(R.id.editText_Tema);
+        editText_Fecha = findViewById(R.id.editText_Fecha);
+        spinner_Carrera = findViewById(R.id.spinner_Carrera);
+        spinner_Asignatura = findViewById(R.id.spinner_Asignatura);
     }
 }
