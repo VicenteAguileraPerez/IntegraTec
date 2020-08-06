8dc4a6c0a72d8c997e3f54d67415368774f7affc
Antonio Pulido <antonio.pulidovirrueta@outlook.com>
2020-08-05 23:04:27 -0500

    [Mod]CreateCodeQRActivity

--- app/src/main/java/com/vicenteaguilera/integratec/CreateCodeQRActivity.java
+++ app/src/main/java/com/vicenteaguilera/integratec/CreateCodeQRActivity.java
@@ -3,10 +3,12 @@ package com.vicenteaguilera.integratec;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.DatePickerDialog;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -15,28 +17,32 @@ import com.google.android.material.snackbar.Snackbar;
 
 import net.glxn.qrgen.android.QRCode;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class CreateCodeQRActivity extends AppCompatActivity {
 
     private EditText editText_Nombre;
     private EditText editText_Tema;
-    private EditText editText_Fecha;
     private Spinner spinner_Carrera;
     private Spinner spinner_Asignatura;
     private ImageView imageView;
     private CardView cardView_BtnCrearQR;
 
+    private Calendar myCalendar = Calendar.getInstance();
+
     private final String [] MATERIAS  ={"Seleccione una materia...","Álgebra", "Álgebra lineal", "Cálculo diferencial", "Cálculo integral", "Cálculo vectorial", "Ecuaciones diferenciales", "Química", "Física"};
-    private final String [] CARRERAS ={"Seleccione una carrera","Ingeniería en Sistemas Computacionales", "Ingeniería en Administración", "Ingeniería en Mecatrónica", "Ingeniería Industrial", "Ingeniería en Mecánica", "Ingeniería en Industrias Alimentarias", "Ingeniería Civil"};
+    private final String [] CARRERAS ={"Seleccione una carrera...","Ingeniería en Sistemas Computacionales", "Ingeniería en Administración", "Ingeniería en Mecatrónica", "Ingeniería Industrial", "Ingeniería en Mecánica", "Ingeniería en Industrias Alimentarias", "Ingeniería Civil"};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_code_q_r);
 
         editText_Nombre = findViewById(R.id.editText_Nombre);
         editText_Tema = findViewById(R.id.editText_Tema);
-        editText_Fecha = findViewById(R.id.editText_Fecha);
         spinner_Carrera = findViewById(R.id.spinner_Carrera);
         spinner_Asignatura = findViewById(R.id.spinner_Asignatura);
         imageView = findViewById(R.id.imageView);
@@ -56,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 crearQR();
             }
         });
+
     }
 
     private void crearQR()
@@ -68,21 +75,13 @@ public class MainActivity extends AppCompatActivity {
                 {
                     if(!editText_Tema.getText().toString().isEmpty())
                     {
-                        if(!editText_Fecha.getText().toString().isEmpty())
-                        {
 
-                            String texto = editText_Nombre.getText().toString() + "_" + spinner_Carrera.getSelectedItem().toString() + "_"
-                                    + spinner_Asignatura.getSelectedItem().toString() + "_" + editText_Tema.getText().toString() + "_"
-                                    + editText_Fecha.getText().toString();
+                        String texto = editText_Nombre.getText().toString() + "_" + spinner_Carrera.getSelectedItem().toString() + "_" +
+                                spinner_Asignatura.getSelectedItem().toString() + "_" + editText_Tema.getText().toString();
 
-                            Bitmap bitmap = QRCode.from(texto).withSize(300, 300).bitmap();
-                            imageView.setImageBitmap(bitmap);
-                            imageView.setVisibility(View.VISIBLE);
-                        }
-                        else
-                        {
-                            Snackbar.make(findViewById(android.R.id.content), "Debes ingresar la fecha de asesoria.", Snackbar.LENGTH_SHORT).show();
-                        }
+                        Bitmap bitmap = QRCode.from(texto).withSize(300, 300).bitmap();
+                        imageView.setImageBitmap(bitmap);
+                        imageView.setVisibility(View.VISIBLE);
                     }
                     else
                     {
@@ -104,4 +103,7 @@ public class MainActivity extends AppCompatActivity {
             Snackbar.make(findViewById(android.R.id.content), "Debes ingresar tú nombre.", Snackbar.LENGTH_SHORT).show();
         }
     }
+
+
+
 }
