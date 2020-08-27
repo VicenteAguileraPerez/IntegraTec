c165977e1d2b30090db1c10fc9947618886ce259
Antonio Pulido <antonio.pulidovirrueta@outlook.com>
2020-08-26 22:16:11 -0500

    [Mod]saveImage

--- app/src/main/java/com/vicenteaguilera/integratec/CreateCodeQRActivity.java
+++ app/src/main/java/com/vicenteaguilera/integratec/CreateCodeQRActivity.java
@@ -183,7 +183,7 @@ public class CreateCodeQRActivity extends AppCompatActivity {
             ContentValues contentValues = new ContentValues();
             contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name + ".jpg");
             contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg");
-            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM);
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS);
             Uri imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
             outputStream = contentResolver.openOutputStream(Objects.requireNonNull(imageUri));
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
