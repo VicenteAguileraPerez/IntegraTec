cf4d681c9b17a4016d39a67ad046ae1ef33fccf9
Antonio Pulido <antonio.pulidovirrueta@outlook.com>
2020-08-27 21:31:38 -0500

    [Mod]Cardview para eliminar los pequeños puntos blancos y spinner semestres

--- app/src/main/java/com/vicenteaguilera/integratec/helpers/utility/PropiertiesHelper.java
+++ app/src/main/java/com/vicenteaguilera/integratec/helpers/utility/PropiertiesHelper.java
@@ -5,7 +5,7 @@ public class PropiertiesHelper {
     public static final String [] MATERIAS  ={"Seleccione una materia...","Álgebra", "Álgebra lineal", "Cálculo diferencial", "Cálculo integral", "Cálculo vectorial", "Ecuaciones diferenciales", "Química", "Física"};
     public static final String [] LUGARES ={"Seleccione un lugar...","Biblioteca", "Telemática", "Edificio A", "Edificio F","Otro(Especifique)"};
     public static final String [] CARRERAS ={"Seleccione una carrera...","Ingeniería en Sistemas Computacionales", "Ingeniería en Administración", "Ingeniería en Mecatrónica", "Ingeniería Industrial", "Ingeniería en Mecánica", "Ingeniería en Industrias Alimentarias", "Ingeniería Civil"};
-    public static final String [] SEMESTRES ={"Seleccione un semestre..","Primero A", "Primero B", "Segundo A", "Segundo B", "Tercero A", "Tercero B", "Cuarto A", "Cuarto B", "Quinto A", "Quinto B", "Sexto A", "Sexto B", "Séptimo A", "Séptimo B", "Octavo A", "Octavo B", "Noveno A", "Noveno B", "Decimo A", "Decimo B"};
+    public static final String [] SEMESTRES ={"Seleccione un semestre..","1A", "1B", "2A", "2B", "3A", "3B", "4A", "4B", "5A", "5B", "6A", "6B", "7A", "7B", "8A", "8B", "9A", "9B", "10A", "10B"};
     public static final String CODIGO_CB = "IaTmSpUit4";
     public static final String EMAIL ="integratecitsu@gmail.com";
     public static final String PASSWORD = "IntegraTec1234";
